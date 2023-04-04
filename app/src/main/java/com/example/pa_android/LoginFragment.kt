b1d54f1cee67556a606e20f97c479ca2be5ada57
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-04-04 08:38:04 +0200

    restructuration de projet et mise en place de la navigation

---
+++ app/src/main/java/com/example/pa_android/LoginFragment.kt
@@ -0,0 +1,30 @@
+package com.example.pa_android
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.hide()
+    }
+}
