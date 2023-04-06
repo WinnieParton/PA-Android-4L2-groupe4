8057a22fe7f2b428da4a1b835db033d484c0914b
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-04-06 14:32:44 +0200

    finish screem rehister

--- app/src/main/java/com/example/pa_android/SignupFragment.kt
+++ app/src/main/java/com/example/pa_android/SignupFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 
 class SignupFragment : Fragment() {
@@ -15,7 +17,20 @@ class SignupFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        val view = inflater.inflate(R.layout.fragment_signup, container, false)
+
+        view.findViewById<TextView>(R.id.buttoncancel).setOnClickListener {
+            findNavController().navigate(
+                SignupFragmentDirections.actionSignupFragmentToLoginFragment()
+            )
+        }
+        view.findViewById<TextView>(R.id.buttonvalide).setOnClickListener {
+            findNavController().navigate(
+                SignupFragmentDirections.actionSignupFragmentToHomeFragment()
+            )
+        }
+
+        return view
     }
 
     override fun onResume() {
