94eb8b8213647a9dece4e5e4022d917aa91f303b
Winnie Koum <wkoumwonkoagni@myges.fr>
2023-04-06 15:14:50 +0200

    add menu bottom

--- app/src/main/java/com/example/pa_android/HomeFragment.kt
+++ app/src/main/java/com/example/pa_android/HomeFragment.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomeFragment : Fragment() {
@@ -14,8 +18,32 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        view.findViewById<BottomNavigationView>(R.id.bottomNavigationView).background = null
+        view.findViewById<BottomNavigationView>(R.id.bottomNavigationView).menu.getItem(2).isEnabled = false
+        return view
+    }
+    override fun onResume() {
+        super.onResume()
+        (activity as AppCompatActivity?)!!.supportActionBar!!.show()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+        val onBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                /*findNavController().navigate(
+                    HomeFragmentDirections.actionGameHomeFragmentToGameLoginFragment()
+                )*/
+            }
+
+
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
